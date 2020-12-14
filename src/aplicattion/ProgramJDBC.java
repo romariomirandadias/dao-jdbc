28d9db7050a5136aee189bffe6fa892f054c7e76
romariomirandadias <rmirandadias@hotmail.com>
2020-12-14 09:46:59 -0300

    Departamento

---
+++ src/aplicattion/ProgramJDBC.java
@@ -0,0 +1,13 @@
+package aplicattion;
+
+import model.entities.Departamento;
+
+public class ProgramJDBC {
+
+	public static void main(String[] args) {
+		Departamento obj=new Departamento(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
