3f23acd0d017269ed0a5cfa37d9ed42bae22c53f
romariomirandadias <rmirandadias@hotmail.com>
2020-12-14 23:26:37 -0300

    VendedorDao e DaoFactory

--- src/aplicattion/ProgramJDBC.java
+++ src/aplicattion/ProgramJDBC.java
@@ -2,14 +2,17 @@ package aplicattion;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class ProgramJDBC {
 
 	public static void main(String[] args) {
-		Departamento obj=new Departamento(1, "Books");
-		Vendedor v=new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 1800.00, obj);
+		Departamento obj = new Departamento(1, "Books");
+		Vendedor v = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 1800.00, obj);
+		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
 		System.out.println(v);
 
 	}
